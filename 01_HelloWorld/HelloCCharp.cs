9133e3c6f4ba1f40be8131b2195e06e5d30629bd
Joseph Enev <yosif.enev@gmail.com>
2015-01-10 01:40:18 +0200

    Introducing To Programming
    First Homework

---
+++ 01_HelloWorld/HelloCCharp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+/*
+ * Write (Hello, C#)
+ */
+class HelloCCharp
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, C#");
+    }
+}
+
---
+++ 04_FullName/Fullname.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+/*
+ * Create console application that prints your first and last name, each at a separate line.
+ */
+
+class FullName
+{
+    static void Main()
+    {
+        Console.WriteLine("Leo");
+        Console.WriteLine("DiCaprio");
+    }
+}
+
